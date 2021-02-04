fc3d3040a4bb946de2239ff95ef64a482ac6db59
zreal <2504846414@qq.com>
2021-02-04 21:58:31 +0800

    chore: 调整了一下

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,20 +2,30 @@ module.exports = {
   title: "一个靓仔的博客",
   description: "Just playing around",
   themeConfig: {
+    repo: "https://github.com/zreal-leo/zreal-leo.github.io",
+    editLinks: true,
+    editLinkText: "编辑此页",
+    docsRepo: "zreal-leo/zreal-leo.github.io",
+    docsDir: "docs",
+    docsBranch: "main",
     sidebar: [
       {
-        title: "JS", // 必要的
-        collapsable: false, // 可选的, 默认值是 true,
-        sidebarDepth: 1, // 可选的, 默认值是 1
+        title: "JS",
+        collapsable: false,
+        sidebarDepth: 1,
         children: ["/js/任务队列", "/js/继承"],
       },
       {
         title: "tools",
-        collapsable: false, // 可选的, 默认值是 true,
-        sidebarDepth: 1, // 可选的, 默认值是 1
+        collapsable: false,
+        sidebarDepth: 1,
         children: ["tools/axios封装", "tools/生成changelog"],
-        // initialOpenGroupIndex: -1, // 可选的, 默认值是 0
       },
     ],
+    lastUpdated: "更新于",
   },
+  markdown: {
+    lineNumbers: true,
+  },
+  evergreen: true,
 }
