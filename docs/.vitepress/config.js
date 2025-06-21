014fb97eb8c7c64032165a2f5896bc750b915382
zreal <real_row@163.com>
2025-06-21 15:37:13 +0800

    chore: 调整菜单结构

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -6,6 +6,13 @@ export default {
     lineNumbers: true
   },
   themeConfig: {
+    search: {
+      provider: "local"
+    },
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "其他", link: "/others/" }
+    ],
     socialLinks: [
       {
         icon: "github",
@@ -17,78 +24,45 @@ export default {
         "https://github.com/zreal-leo/zreal-leo.github.io/edit/main/docs/:path",
       text: "编辑此页"
     },
-    sidebar: [
-      {
-        text: "JS",
-        items: [
-          {
-            text: "任务队列",
-            link: "/js/任务队列.md"
-          },
-          {
-            text: "继承",
-            link: "/js/继承.md"
-          },
-          {
-            text: "手写Promise",
-            link: "/js/手写Promise.md"
-          },
-          {
-            text: "迭代器与生成器",
-            link: "/js/迭代器与生成器.md"
-          },
-          {
-            text: "柯里化以及偏函数",
-            link: "/js/柯里化以及偏函数.md"
-          },
-          {
-            text: "Proxy 和 Reflect",
-            link: "/js/ProxyReflect.md"
-          }
-        ]
-      },
-      {
-        text: "Tools",
-        items: [
-          {
-            text: "axios 封装",
-            link: "/tools/axios封装.md"
-          },
-          {
-            text: "commit生成changelog",
-            link: "/tools/生成changelog.md"
-          }
-        ]
-      },
-      {
-        text: "其他",
-        items: [
-          {
-            text: "ts 初识",
-            link: "/ts/初识.md"
-          },
-          {
-            text: "函数式调用组件",
-            link: "/vue/函数调用组件.md"
-          },
-          {
-            text: "Hook",
-            link: "/others/Hook.md"
-          },
-          {
-            text: "webSocket",
-            link: "/tools/websocket.md"
-          },
-          {
-            text: "跨域",
-            link: "/others/跨域.md"
-          },
-          {
-            text: "monoRepo",
-            link: "/others/monoRepo.md"
-          }
-        ]
-      }
-    ]
+    sidebar: {
+      "/": [
+        {
+          text: "JS",
+          items: [
+            { text: "任务队列", link: "/js/任务队列" },
+            { text: "继承", link: "/js/继承" },
+            { text: "手写Promise", link: "/js/手写Promise" },
+            { text: "迭代器与生成器", link: "/js/迭代器与生成器" },
+            { text: "柯里化以及偏函数", link: "/js/柯里化以及偏函数" },
+            { text: "Proxy 和 Reflect", link: "/js/ProxyReflect" }
+          ]
+        },
+        {
+          text: "Vue",
+          items: [{ text: "函数式调用组件", link: "/vue/函数调用组件" }]
+        }
+      ],
+      "/others/": [
+        {
+          text: "工程化",
+          items: [{ text: "monoRepo", link: "/others/monoRepo" }]
+        },
+        {
+          text: "Tools",
+          items: [
+            { text: "commit生成changelog", link: "/others/生成changelog" },
+            { text: "axios 封装", link: "/others/axios封装" },
+            { text: "webSocket", link: "/others/websocket" }
+          ]
+        },
+        {
+          text: "其他技术",
+          items: [
+            { text: "Hook", link: "/others/Hook" },
+            { text: "跨域", link: "/others/跨域" }
+          ]
+        }
+      ]
+    }
   }
 };
